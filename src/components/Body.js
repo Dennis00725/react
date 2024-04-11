c41c3adfc167b931b6731be9bfdc8b3424f2811d
Dennis00725 <dennis00725@gmail.com>
2024-04-11 20:11:20 +0530

    sorted top restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,23 +4,26 @@ import { useState } from "react";
 
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+
+  const filterTopRatedRestaurants = () => {
+    const filteredList = listOfRestaurant.map((listItem) => ({
+      ...listItem,
+      restaurants: listItem.restaurants.filter(
+        (restaurant) => parseFloat(restaurant.avgRatingString) > 4
+      ),
+    }));
+    setListOfRestaurant(filteredList);
+  };
+
   return (
     <div className="body">
       <div className="filter">
-        <button
-          className="fltr-btn"
-          onClick={() => {
-            const filteredList = listOfRestaurant.filter(
-              (restaurant) => restaurant.avgRatingString > 4
-            );
-            setListOfRestaurant(filteredList);
-          }}
-        >
-          Top-Rated Restaurant
+        <button className="fltr-btn" onClick={filterTopRatedRestaurants}>
+          Top-Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {resList.map((listItem) =>
+        {listOfRestaurant.map((listItem) =>
           listItem.restaurants.map((restaurant) => (
             <RestaurantCard key={restaurant.id} resData={restaurant} />
           ))
