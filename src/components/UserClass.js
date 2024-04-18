38f73fbc97ba590f80a772adf7fd535ae93caa05
Dennis00725 <dennis00725@gmail.com>
2024-04-18 17:01:31 +0530

    added class-based component to About.js

---
+++ src/components/UserClass.js
@@ -0,0 +1,13 @@
+import React from "react";
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: Dennis</h2>
+        <h3>contact : @dennis00725</h3>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
