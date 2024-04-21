85d42cd8508cbeed6f52d45ca2e3a30d433f1fe5
Dennis00725 <dennis00725@gmail.com>
2024-04-21 11:36:18 +0530

    created 2 states in class-based components in UserClass

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,10 +1,22 @@
 import React from "react";
 class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
+  }
+
   render() {
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
+        <h1>count:{count}</h1>
+        <h1>count:{count2}</h1>
         <h2>Name: Dennis</h2>
-        <h3>contact : @dennis00725</h3>
+        <h3></h3>
+        <h3>Contact : @dennis00725</h3>
       </div>
     );
   }
--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -1,8 +1,5 @@
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const LOGO_URL =
-  "https://r2.erweima.ai/ideogram/kkPw7ZAcSf-OCJqNcT6Hww.jpg";
-
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=9.9312328&lng=76.26730409999999&restaurantId=";
